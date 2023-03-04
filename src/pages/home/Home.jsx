ad02cc08ba1c92d7a004948796835e8d349c821b
Tyler Coats <coatsctyler@gmail.com>
2023-03-04 14:28:15 -0500

    Finished basic home jsx and css

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,34 @@
+import './Home.css'
+import { BsFillArrowRightCircleFill } from 'react-icons/bs'
+import sous from '../../imgs/home/sousChef.png'
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home-ctn">
+        <div className="home-left">
+            <div className='home-left-header'>
+                <img src={sous} alt="" />
+            </div>
+            <div className='home-left-content'>
+                <div className='home-left-title'>
+                    <h1>Welcome to <span>Sous Chef</span></h1>
+                </div>
+                <div className='home-left-text'>
+                    <h2>Let's Get Cooking</h2>
+                    <h3>The one stop shop for all your favorite recipes</h3>
+                    <button>Find Recipes <span><BsFillArrowRightCircleFill /></span> </button>
+                </div>
+                <div className='home-left-imgs'>
+                    <div className='img-card sandwich'></div>
+                    <div className='img-card sandwich'></div>
+                    <div className='img-card sandwich'></div>
+                </div>
+            </div>
+        </div>
+        <div className="home-right">
+
+        </div>
+    </div>
   )
 }
 export default Home
