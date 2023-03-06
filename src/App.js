1a55c851c6e97aa98579f9a068cdf29210a8e182
Tyler Coats <coatsctyler@gmail.com>
2023-03-06 16:11:01 -0500

    Removed nav

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ function App() {
         
 
       {/* HELLO WORKING PAGE */}
-    <Nav />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/dashboard' element={<Dashboard />} />
