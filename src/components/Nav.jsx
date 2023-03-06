13a8eac614385564781d950801dfe3031e2ea933
Tyler Coats <coatsctyler@gmail.com>
2023-03-06 13:55:05 -0500

    Added nav css

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,21 @@
 import { Link } from 'react-router-dom'
+import sous from '../imgs/home/sousChef.png'
 
 const Nav = () => {
 
   return (
-    <header>
-      <nav className='links'>
-        <h3 className='link-title'>Thrill RideShare</h3>
-        <div>
-          <Link to="/dashboard" className="inactive" activeclassname="active">Home</Link>
-          <Link to="/Favorites" className="inactive" activeclassname="active">Favorites</Link>
-          <Link to="/About" className="inactive" activeclassname="active">About</Link>
-          <Link to="/Login" className="inactive" activeclassname="active">Login</Link>
-        </div>
-      </nav>
-    </header>
+    <nav className='links'>
+      <div className='logo'>
+        <img src={sous} alt="" />
+        <h3 className='link-title'>Sous Chef</h3>
+      </div>
+      <div className='nav-list'>
+        <Link to="/dashboard" className="inactive" activeclassname="active">Home</Link>
+        <Link to="/Favorites" className="inactive" activeclassname="active">Favorites</Link>
+        <Link to="/About" className="inactive" activeclassname="active">About</Link>
+        <Link to="/Login" className="inactive" activeclassname="active">Login</Link>
+      </div>
+    </nav>
   )
 }
 
