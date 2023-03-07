e26ac78f3c8acbe9b469a3ead5a963d0a8f09fbd
Tyler Coats <coatsctyler@gmail.com>
2023-03-07 09:10:22 -0500

    Added mealDetails html,js,css / Added about page html,css,js

--- src/pages/mealDetails/MealDetails.jsx
+++ src/pages/mealDetails/MealDetails.jsx
@@ -1,29 +1,73 @@
 import { useParams, NavLink, useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import './MealDetails.css'
+import Nav from '../../components/Nav'
+import { Link } from 'react-router-dom'
 
 
+const MealDetails = ({ meal }) => {
+  let { id } = useParams()
+  const [mealDetails, setMealDetails] = useState({})
 
-const MealDetails = ({meal}) => {
-    let { id } = useParams()
-    const [mealDetails, setMealDetails] = useState({})
+  const [toggleTabs, setToggleTabs] = useState(1)
 
-    const getMealDetail = async () => {
-        try {
-          const res = await axios.get(`http://localhost:3001/meals/${ id }`)
-          setMealDetails(res.data.meal)
-          console.log(res.data.meal);
-        } catch (err) {
-          console.log(err)
-        }
-      }
+  const tabChanger = (i) => {
+    setToggleTabs(i)
+  }
 
-    useEffect(() => {
-        getMealDetail()
-      }, [meal])
+  const getMealDetail = async () => {
+    try {
+      const res = await axios.get(`http://localhost:3001/meals/${id}`)
+      setMealDetails(res.data.meal)
+      console.log(res.data.meal);
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    getMealDetail()
+  }, [meal])
 
   return (
-    <div>name: {mealDetails.name}</div>
+    <div className='mealDetailsCtn'>
+      <Nav />
+      <div className='mealDetailsLeft'>
+          <img src={mealDetails.picture} alt="" />
+      </div>
+      <div className='mealDetailsRight'>
+        <div className='backBtn'>
+          <Link to={'/dashboard'} className='backLink'><p>Back</p></Link>
+        </div>
+        <div className='mealNameDesc'>
+          <h1>{mealDetails.name}</h1>
+          <h2>{mealDetails.description}</h2>
+        </div>
+        <div className='mealTabCnt'>
+          <div className={toggleTabs === 1 ? "mealTabs activeTab" : "mealTabs"} onClick={() => tabChanger(1)}>
+            <h3>Time</h3>
+          </div>
+          <div className={toggleTabs === 2 ? "mealTabs activeTab" : "mealTabs"} onClick={() => tabChanger(2)}>
+            <h3>Ingredients</h3>
+          </div>
+          <div className={toggleTabs === 3 ? "mealTabs activeTab" : "mealTabs"} onClick={() => tabChanger(3)}>
+            <h3>Recipe</h3>
+          </div>
+          <div className='mealTabTextCtn'>
+              <div className={toggleTabs === 1 ? "mealTabText activeText" : "mealTabText"}>
+                <p>{mealDetails.cook_time} Minutes</p>
+              </div>
+              <div className={toggleTabs === 2 ? "mealTabText activeText" : "mealTabText"}>
+                <p>{mealDetails.ingredients}</p>
+              </div>
+              <div className={toggleTabs === 3 ? "mealTabText activeText" : "mealTabText"}>
+                <p></p>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 export default MealDetails
