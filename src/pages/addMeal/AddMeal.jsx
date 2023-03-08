bf07edffac518cc1d957353a05388e5c419e623d
Tyler Coats <coatsctyler@gmail.com>
2023-03-08 10:25:04 -0500

    Adding to pull

--- src/pages/addMeal/AddMeal.jsx
+++ src/pages/addMeal/AddMeal.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import './AddMeal.css'
+import Nav from '../../components/Nav'
 
 const AddMeal = () => {
-  
+
   const navigate = useNavigate()
 
   const initialState = {
-    createdby:'15',
+    createdby: '15',
     name: '',
     description: '',
     cooktime: '',
@@ -15,25 +17,25 @@ const AddMeal = () => {
     diet: '',
     ingredients: '',
     mealtype: '',
-    image:''
+    image: ''
   }
-  
+
   const [formValues, setFormValues] = useState(initialState)
-  
+
   const handleChange = (e) => {
     setFormValues({ ...formValues, [e.target.name]: e.target.value })
   }
 
   const addMeal = async (data) => {
     try {
-        const res = await axios.post('http://localhost:3001/meals/create', data)
-        navigate('/dashboard')
-        return res.data
+      const res = await axios.post('http://localhost:3001/meals/create', data)
+      navigate('/dashboard')
+      return res.data
     } catch (error) {
-        console.log(error);
-        throw error
+      console.log(error);
+      throw error
     }
-}
+  }
 
   const onSubmit = (e) => {
     e.preventDefault()
@@ -43,70 +45,71 @@ const AddMeal = () => {
 
 
   return (
-    <div>
-    <div>
-      <h1>AddMeal</h1>
-      <form  onSubmit={onSubmit}>
-        <label htmlFor="name">Name*</label>
-        <input 
-          name="name"
-          type="text"
-          placeholder="Meal Name ex. Nachos"
-          onChange={handleChange}
-          required
-        />
-        <label htmlFor="description">Description*</label>
-        <input 
-          name="description"
-          type="text"
-          placeholder="Describe your food"
-          onChange={handleChange}
-          required
-        />
-        <label htmlFor="cooktime">Cook Time</label>
-        <input 
-          name="cooktime"
-          type="text"
-          placeholder="in hours ex. 1"
-          onChange={handleChange}
-        />
-        <label htmlFor="cuisine">Cuisine</label>
-        <input 
-          name="cuisine"
-          type="text"
-          placeholder="Mexican, Italian, etc."
-          onChange={handleChange}
-        />
-        <label htmlFor="diet">Diet Type</label>
-        <input 
-          name="diet"
-          type="text"
-          placeholder=""
-          onChange={handleChange}
-        />
-        <label htmlFor="ingredients">Ingredients</label>
-        <input 
-          name="ingredients"
-          type="text"
-          placeholder=""
-          onChange={handleChange}
-        />
-        <label htmlFor="mealtype">Meal type</label>
-        <input 
-          name="mealtype"
-          type="text"
-          placeholder="Breakfast? Lunch?"
-          onChange={handleChange}
-        />
-        <label htmlFor="image">Image Url</label>
-        <input 
-          name="image"
-          type="text"
-          onChange={handleChange}
-        />
-      <button type="submit">Submit</button>
-      </form>
-      
+    <div className='add-page-ctn'>
+      <Nav />
+      <div className='add-page-div'>
+        <h1>AddMeal</h1>
+        <form onSubmit={onSubmit}>
+          <label htmlFor="name">Name*</label>
+          <input
+            name="name"
+            type="text"
+            placeholder="Meal Name ex. Nachos"
+            onChange={handleChange}
+            required
+          />
+          <label htmlFor="description">Description*</label>
+          <input
+            name="description"
+            type="text"
+            placeholder="Describe your food"
+            onChange={handleChange}
+            required
+          />
+          <label htmlFor="cooktime">Cook Time</label>
+          <input
+            name="cooktime"
+            type="text"
+            placeholder="in hours ex. 1"
+            onChange={handleChange}
+          />
+          <label htmlFor="cuisine">Cuisine</label>
+          <input
+            name="cuisine"
+            type="text"
+            placeholder="Mexican, Italian, etc."
+            onChange={handleChange}
+          />
+          <label htmlFor="diet">Diet Type</label>
+          <input
+            name="diet"
+            type="text"
+            placeholder=""
+            onChange={handleChange}
+          />
+          <label htmlFor="ingredients">Ingredients</label>
+          <input
+            name="ingredients"
+            type="text"
+            placeholder=""
+            onChange={handleChange}
+          />
+          <label htmlFor="mealtype">Meal type</label>
+          <input
+            name="mealtype"
+            type="text"
+            placeholder="Breakfast? Lunch?"
+            onChange={handleChange}
+          />
+          <label htmlFor="image">Image Url</label>
+          <input
+            name="image"
+            type="text"
+            onChange={handleChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+
       </div>
     </div>
   )
