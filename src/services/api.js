891230504884f9f7f4978ff416fe35f9c0f9457b
Tyler Coats <coatsctyler@gmail.com>
2023-03-09 20:21:18 -0500

    Changed nav and adjusted css

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 import Axios from 'axios'
 
-export const BASE_URL = 'https://sous-chef.herokuapp.com'
+// export const BASE_URL = 'https://sous-chef.herokuapp.com'
+
+export const BASE_URL = 'http://localhost:3001/'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
