af46103e1ce0e14f7e70182b5726386f334491b4
Tyler Coats <coatsctyler@gmail.com>
2023-03-18 18:55:18 -0400

    Added nav changes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import { Link, useNavigate } from 'react-router-dom'
 import sous from '../imgs/home/sousChef.png'
+import { bubble as Menu } from 'react-burger-menu';
 
 const Nav = ({ user, setUser }) => {
 
@@ -12,22 +13,38 @@ const Nav = ({ user, setUser }) => {
   }
 
   return (
-    <nav className='links'>
-      <div className='logo' onClick={()=>navigate('/dashboard')}>
-          <img src={sous} alt="" />   
-        <h3 className='link-title'>Sous Chef</h3>
-      </div>
-      <div className='nav-list'>
-        <Link to="/dashboard" className="inactive" activeclassname="active">Home</Link>
-        {user ? <Link to="/auth" className="inactive" activeclassname="active">My Meals</Link>
-          : <Link to="/Login" className="inactive" activeclassname="active">My Meals</Link>
-        }
-        <Link to="/About" className="inactive" activeclassname="active">About</Link>
-        {user ? <a style={{ cursor: "pointer" }} className="inactive" activeclassname="active" onClick={() => handleLogOut()}>Logout</a>
-          : <Link to="/Login" className="inactive" activeclassname="active">Login</Link>
-        }
-      </div>
-    </nav>
+    <div style={{ width: '100%' }}>
+      <nav className='links'>
+
+        <div className='logo' onClick={() => navigate('/dashboard')}>
+          <img src={sous} alt="" />
+          <h3 className='link-title'>Sous Chef</h3>
+        </div>
+        <Menu right>
+          <Link to="/dashboard" className="inactive" activeclassname="active">Home</Link>
+          {user ? <Link to="/auth" className="inactive" activeclassname="active">My Meals</Link>
+            : <Link to="/Login" className="inactive" activeclassname="active">My Meals</Link>
+          }
+          <Link to="/About" className="inactive" activeclassname="active">About</Link>
+          {user ? <a style={{ cursor: "pointer" }} className="inactive" activeclassname="active" onClick={() => handleLogOut()}>Logout</a>
+            : <Link to="/Login" className="inactive" activeclassname="active">Login</Link>
+          }
+        </Menu>
+        <div className='nav-list'>
+          <Link to="/dashboard" className="inactive" activeclassname="active">Home</Link>
+          {user ? <Link to="/auth" className="inactive" activeclassname="active">My Meals</Link>
+            : <Link to="/Login" className="inactive" activeclassname="active">My Meals</Link>
+          }
+          <Link to="/About" className="inactive" activeclassname="active">About</Link>
+          {user ? <a style={{ cursor: "pointer" }} className="inactive" activeclassname="active" onClick={() => handleLogOut()}>Logout</a>
+            : <Link to="/Login" className="inactive" activeclassname="active">Login</Link>
+          }
+          
+        </div>
+      </nav>
+
+
+    </div>
   )
 }
 
